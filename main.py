8845e0e7fa750857e889c7966ed5fc37186be246
Filip Bartek <filip.bartek@hotmail.com>
2020-02-20 19:52:04 +0100

    Add a scoring procedure

--- main.py
+++ main.py
@@ -24,6 +24,22 @@ def save_result(filename, libraries):
             f.write('\n')
 
 
+def score(solution, days, book_scores, library_signup_times, library_ship_capacities):
+    total = 0
+    current_time = 0
+    for library, books in solution:
+        signup_time = library_signup_times[library]
+        ship_capacity = library_ship_capacities[library]
+        current_time += signup_time
+        remaining_time = days - current_time
+        n_books_to_be_scanned = remaining_time * ship_capacity
+        books_to_be_scanned = books[:n_books_to_be_scanned]
+        cur_scores = book_scores[books_to_be_scanned]
+        total += np.sum(cur_scores)
+        book_scores[books_to_be_scanned] = 0
+    return total
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--iterations', type=int, default=100)
@@ -36,4 +52,9 @@ if __name__ == '__main__':
     for f in glob.glob('input/*.txt'):
         print(f)
         b, l, d, s, libraries = load_file.load_libraries(f)
-        save_result(f'{f}.out', [(1, [5, 2, 3]), (0, [0, 1, 2, 3, 4])])
+        s = np.asarray(s, dtype=np.uint)
+        library_signup_times = [lib[1] for lib in libraries]
+        library_ship_capacities = [lib[2] for lib in libraries]
+        solution = [(1, [5, 2, 3]), (0, [0, 1, 2, 3, 4])]
+        print(score(solution, d, s, library_signup_times, library_ship_capacities))
+        save_result(f'{f}.out', solution)
