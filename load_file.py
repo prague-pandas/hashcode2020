e464e261bfd5d1421d91ffa83c8693a3a67dea45
Jogisara Ramachandra Sandesh Bhat <sandeshbhatjr@gmail.com>
2020-02-20 19:11:05 +0100

    Add a custom loader for the competition

--- load_file.py
+++ load_file.py
@@ -11,3 +11,35 @@ def load_file(filename, line, word=None, line_separator='\n', word_separator=' '
 
 def input_files(folder_name='input', extension='.in'):
 	yield list(filter(lambda x: x.endswith(extension), os.listdir(folder_name)))
+
+# this is the custom loader for the competition
+def load_input(filename):
+	num_diff_books = load_file(filename, 0, word=0)
+	num_libraries = load_file(filename, 0, word=1)
+	num_days = load_file(filename, 0, word=2)
+
+	book_scores = load_file(filename, 1)
+
+	library = []
+
+	for i in range(num_libraries):
+		temp_library_dict = {}
+
+		library_line = (2*i) + 2
+		num_books = load_file(filename, library_line, word=0)
+		num_days_for_signup = load_file(filename, library_line, word=1)
+		num_books_shippable = load_file(filename, library_line, word=2)
+
+		book_ids = load_file(filename, library_line + 1)
+
+		temp_library_dict['num of books'] = num_books
+		temp_library_dict['num of days'] = num_days_for_signup
+		temp_library_dict['num of books shippable'] = num_books_shippable
+		temp_library_dict['book id list'] = book_ids
+
+		library.append(temp_library_dict)
+
+	return num_diff_books, num_libraries, num_days, book_scores, library
+
+
+
